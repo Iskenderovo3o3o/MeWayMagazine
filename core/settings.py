4decd412def803cf1d5ae0c3cf53e42c82685937
Ysak-Emir <emirchik.isak@gmail.com>
2023-05-09 18:48:41 +0600

    postfres add

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 
@@ -62,8 +63,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'USER': os.environ.get("DB_USER"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': '5432'
     }
 }
 
